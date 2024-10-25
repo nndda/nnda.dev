d5276956c94c13bea2f8fd0807f9652fd914346a
nnda <96333146+nndda@users.noreply.github.com>
2024-10-25 15:37:26 +0800

    Implement clock hands

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,38 +1,63 @@
 const CircleType = require("circletype");
 
+function rotateElem(elem : HTMLElement, degrees : number) {
+  let currentRotation = parseInt(<string>elem.getAttribute("data-rotation"), 10);
+  if (currentRotation >= 3600) {
+    currentRotation = 0 + degrees;
+  } else {
+    currentRotation += degrees;
+  }
+
+  elem.setAttribute("data-rotation", currentRotation.toString());
+  elem!.style.transform = `translate(-50%, -50%) rotate(${currentRotation}deg)`;
+}
+
+const thingsStuffStr = <string[]>[
+  "Game Developer",
+  "Front-end Developer",
+  "Open Source Developer",
+  "Digital Illustrator",
+];
+
 document.addEventListener("DOMContentLoaded", function() {
-  document.querySelectorAll(".bg-spiral").forEach((elem) => {
+  document.querySelectorAll(".bg-spiral:not(.horizontal)").forEach((elem) => {
     new CircleType(elem);
   });
 
   const bgSpiral1 = <HTMLElement>document.getElementById("bg-spiral-1");
   const bgSpiral2 = <HTMLElement>document.getElementById("bg-spiral-2");
 
-  function rotateElem(elem : HTMLElement, degrees : number) {
-    let currentRotation = parseInt(<string>elem.getAttribute("data-rotation"), 10);
-    currentRotation += degrees;
-
-    elem.setAttribute("data-rotation", currentRotation.toString());
-    elem!.style.transform = `translate(-50%, -50%) rotate(${currentRotation}deg)`;
-  }
+  const clockHandSec = <HTMLElement>document.getElementById("clock-hand-sec");
+  const clockHandMin = <HTMLElement>document.getElementById("clock-hand-min");
 
   const thingsStuff = document.getElementById("things-stuff");
-  const thingsStuffStr = <string[]>[
-    "Game Developer",
-    "Front-end Developer",
-    "Open Source Developer",
-    "Digital Illustrator",
-  ];
   let thingsStuffIdx = 0;
 
-  setInterval(function() {
-    rotateElem(bgSpiral1, 9);
-    rotateElem(bgSpiral2, -6);
+  const degSec = 360 / 60;
 
+  let intervalEven = 0;
+
+  rotateElem(clockHandSec, (new Date().getSeconds() - 15) * degSec);
+  rotateElem(clockHandMin, (new Date().getMinutes() - 15) * degSec);
+
+  setInterval(function() {
     thingsStuff!.textContent = thingsStuffStr[thingsStuffIdx];
     thingsStuffIdx = (thingsStuffIdx + 1) % thingsStuffStr.length;
 
-  }, 1200);
+    intervalEven = (intervalEven + 1) % 2;
+
+    rotateElem(clockHandSec, degSec);
+
+    if (intervalEven === 1) {
+      rotateElem(bgSpiral1, degSec * 0.5);
+      rotateElem(bgSpiral2, -degSec);
+    }
+
+    if (new Date().getSeconds() === 0) {
+      rotateElem(clockHandMin, degSec);
+    }
+
+  }, 1000);
 
   const navbarMobile = <HTMLElement>document.getElementById("navbar-mobile");
   const navbarButton = <HTMLButtonElement>document.getElementById("navbar-button");
