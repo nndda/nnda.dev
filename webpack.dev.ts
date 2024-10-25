6b41c65d3313c8ab22e67ae079139eceee9088a3
nnda <96333146+nndda@users.noreply.github.com>
2024-10-25 16:13:44 +0800

    Include WebP format

--- webpack.dev.ts
+++ webpack.dev.ts
@@ -53,7 +53,7 @@ module.exports = {
         use: ["css-loader", "sass-loader"],
       },
       {
-        test: /\.(ico|png|jp?g|svg)/,
+        test: /\.(ico|png|jp?g|webp|svg)$/,
         type: "asset/resource",
       },
     ],
