bb1d4843a31408737ea31fc25e3983bc58882734
nnda <96333146+nndda@users.noreply.github.com>
2025-01-06 05:18:11 +0700

    Implement timezone clock

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -9,7 +9,22 @@ d.addEventListener("DOMContentLoaded", () => {
   const
     navbarMobile = <HTMLElement>d.getElementById("navbar-mobile"),
     navbarButton = <HTMLButtonElement>d.getElementById("navbar-button"),
-    navbarCollapseArea = <HTMLElement>d.getElementById("collapse-trigger-area");
+    navbarCollapseArea = <HTMLElement>d.getElementById("collapse-trigger-area"),
+
+    timezoneClock = <HTMLElement>d.getElementById("clock-timezone"),
+    timezoneOpt: Intl.DateTimeFormatOptions = {
+      timeZone: timezoneClock.getAttribute("data-timezone") ?? "Asia/Jakarta",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+      hour12: false,
+    };
+
+  function updateTimezoneClock() {
+    timezoneClock.textContent = new Intl.DateTimeFormat([], timezoneOpt).format(new Date());
+  }
+
+  setInterval(updateTimezoneClock, 1e3);
 
   function toggleNavMobile(): void {
     navbarMobile.classList.toggle("collapsed");
