115de11cdeba9a30dbce2615cfd389a4d6c8b731
nnda <96333146+nndda@users.noreply.github.com>
2025-01-09 06:51:21 +0700

    Implement on scroll animations

---
+++ src/scripts/animations.ts
@@ -0,0 +1,14 @@
+const observerScrollAnim = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      entry.target.classList.toggle("on", entry.isIntersecting);
+    });
+  },
+  {
+    root: null,
+    threshold: 0.5,
+  }
+);
+
+export function initializeAnimations(d: Document): void {
+  d.querySelectorAll(".anim").forEach(el => observerScrollAnim.observe(el));
+}
