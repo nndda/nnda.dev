b31ed9a5a5be45f9a49906e5d93d2324958842f3
nnda <96333146+nndda@users.noreply.github.com>
2025-01-21 09:41:20 +0700

    Only creates `IntersectionObserver` based on `prefers-reduced-motion`

--- src/scripts/animations.ts
+++ src/scripts/animations.ts
@@ -1,16 +1,16 @@
-const observerScrollAnim = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-      entry.target.classList.toggle("on", entry.isIntersecting);
-    });
-  },
-  {
-    root: null,
-    threshold: 0,
-  }
-);
-
 export function initializeAnimations(d: Document): void {
   if (window.matchMedia("(prefers-reduced-motion: no-preference)").matches) {
+    const observerScrollAnim = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+          entry.target.classList.toggle("on", entry.isIntersecting);
+        });
+      },
+      {
+        root: null,
+        threshold: 0,
+      }
+    );
+
     d.querySelectorAll(".anim").forEach(el => observerScrollAnim.observe(el));
   };
 }
