1b73f9b907054f4e7ec756d600b76eed1cf90b5a
nnda <96333146+nndda@users.noreply.github.com>
2025-02-07 12:16:15 +0700

    Correct package classes

--- src/scripts/build/packages.ts
+++ src/scripts/build/packages.ts
@@ -12,7 +12,7 @@ const packageJSON: any = require("../../../package.json");
 cleanupDir(abs("./"));
 
 writeTextFile(abs("./packages.json"), JSON.stringify(sortBy([
-  ...entries(packageJSON.devDependencies).map(i => i[0] + "d"),
-  ...entries(packageJSON.dependencies).map(i => i[0] + "r"),
+  ...entries(packageJSON.devDependencies).map(i => i[0] + "pkg-d"),
+  ...entries(packageJSON.dependencies).map(i => i[0] + "pkg-r"),
 ])));
 
