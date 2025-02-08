e781ec034d2f4ea8b76eed3ffd03c5851282c1b9
nnda <96333146+nndda@users.noreply.github.com>
2025-02-08 12:27:43 +0700

    Import sorting utility

--- src/views/helpers.ts
+++ src/views/helpers.ts
@@ -10,6 +10,7 @@ import {
   pathResolve,
   createResolver,
 } from "../scripts/build/utils";
+import { sortBy } from "lodash"; // eslint-disable-line
 const srcDir: string = createResolver(__dirname)("../../src/");
 
 function srcResolve(src: string): string {
