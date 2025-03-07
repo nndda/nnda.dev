a67c171f1024c9d73fb15e0c541bdaabf1742f4d
nnda <96333146+nndda@users.noreply.github.com>
2025-03-07 19:02:14 +0700

    Implement one-time enter animation

--- src/scripts/animations.ts
+++ src/scripts/animations.ts
@@ -1,16 +1,21 @@
 export function initializeAnimations(d: Document): void {
-  if (window.matchMedia("(prefers-reduced-motion: no-preference)").matches) {
+  const intersectionObserverOptions: IntersectionObserverInit = {
+    root: null,
+    threshold: 0,
+  };
+
+  if (d.defaultView?.matchMedia("(prefers-reduced-motion: no-preference)").matches) {
     const observerScrollAnim = new IntersectionObserver(entries => {
         entries.forEach(entry => {
           entry.target.classList.toggle("on", entry.isIntersecting);
+
+          if (entry.isIntersecting && entry.target.classList.contains("once")) {
+            observerScrollAnim.unobserve(entry.target);          }
         });
       },
-      {
-        root: null,
-        threshold: 0,
-      }
+      intersectionObserverOptions,
     );
 
     d.querySelectorAll(".anim").forEach(el => observerScrollAnim.observe(el));
-  };
+  }
 }
