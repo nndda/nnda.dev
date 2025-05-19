96acd5ad579f2684d6d88d1edba4236fc7d1f9a9
nnda <96333146+nndda@users.noreply.github.com>
2025-05-20 00:17:51 +0700

    Improve attribution section

--- src/scripts/build/packages.ts
+++ src/scripts/build/packages.ts
@@ -1,6 +1,7 @@
 import _ from "lodash";
 import {
   writeTextFile,
+  readTextFile,
   createResolver,
   type DirResolver
 } from "./utils";
@@ -9,7 +10,17 @@ const abs: DirResolver = createResolver(__dirname);
 import packageJSON from "../../../package.json";
 
 writeTextFile(abs("./packages.json"), JSON.stringify(_.shuffle([
+
   ..._.entries(packageJSON.devDependencies).map(i => i[0] + "pkg-d"),
   ..._.entries(packageJSON.dependencies).map(i => i[0] + "pkg-r"),
+
+  ...readTextFile(abs("../../../config/requirements.txt"))
+    .split("\n")
+    .map(i => i.split("==")[0] + "pythn"),
+
+  ...[
+    "SAWARATSUKI/KawaiiLogos"
+  ].map(i => "   gh"),
+
 ])));
 
