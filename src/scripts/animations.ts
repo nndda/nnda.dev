de058d0aa4a4a85fa556793109443877bfca42bd
nnda <96333146+nndda@users.noreply.github.com>
2025-06-05 10:31:36 +0700

    Use helper for intersection observer

--- src/scripts/animations.ts
+++ src/scripts/animations.ts
@@ -1,31 +1,25 @@
 export function initializeAnimations(d: Document): void {
-  const intersectionObserverOptions: Readonly<IntersectionObserverInit> = Object.freeze({
-    root: null,
-    threshold: 0,
-  });
 
   if ((d.defaultView as Window).matchMedia("(prefers-reduced-motion: no-preference)").matches) {
-    const
-      observerScrollAnim = new IntersectionObserver((entries: IntersectionObserverEntry[]) => {
-          for (let i: number = entries.length; i-- > 0;) {
-            const entry: IntersectionObserverEntry = entries[i];
-            entry.target.classList.toggle("on", entry.isIntersecting);
 
-            if (entry.isIntersecting && entry.target.classList.contains("once")) {
-              observerScrollAnim.unobserve(entry.target);
-            }
+    const
+      observer: (target: Element) => void = window.observe(
+        (
+          entry: IntersectionObserverEntry,
+          observerObj: IntersectionObserver,
+        ): void => {
+          entry.target.classList.toggle("on", entry.isIntersecting);
+
+          if (entry.isIntersecting && entry.target.classList.contains("once")) {
+            observerObj.unobserve(entry.target);
           }
         },
-        intersectionObserverOptions,
       )
-    , observer = observerScrollAnim.observe.bind(observerScrollAnim)
     ;
 
     const animEls: NodeListOf<HTMLElement> = d.querySelectorAll(".anim");
     for (let i: number = animEls.length; i-- > 0;) {
       observer(animEls[i]);
     }
-
-    // d.querySelectorAll(".anim").forEach(observer);
   }
 }
