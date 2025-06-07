9ab9d9bd065fcdaa1149e0719481795934b0d67d
nnda <96333146+nndda@users.noreply.github.com>
2025-06-07 19:57:28 +0700

    Refactor links section & implement loader

--- src/scripts/sections/links.ts
+++ src/scripts/sections/links.ts
@@ -15,54 +15,67 @@ interface LinkGroupItem {
   iconSlug?: string,
 }
 
-export default function (d: Document): void { requestAnimationFrame((): void => {
+const
+  linksStrEl: string =
+    linksData.map((data: LinkGroup): string => {
+      return `
+        <div class="links-group">
+          <div class="links-title anim once fade float-up">${data.group}</div>
+          <ul class="nostyle">
+            ${
+              data.links.map(
+                (link: LinkGroupItem) => {
+                  return `
+                    <li class="
+                        anim once
+                        link-item
+                        ${link.iconSlug || "default"}
+                      "
+                    >
+                      <a href="${link.url}"
+                        target="_blank"
+                        rel="me nofollow noopener noreferrer"
+                        referrerpolicy="no-referrer"
+                        aria-label="${link.name}"
+                      >
+                        <div class="social-icon">
+                          ${link.icon || `<i class="links" data-i="link"></i>`}
+                        </div>
+
+                        <span class="social-name">${link.name}</span>
+
+                        <small class="username">
+                          ${link.username || link.urlS}
+                        </small>
+                      </a>
+                    </li>
+                  `;
+                }
+              ).join("")
+            }
+          </ul>
+        </div>
+        `;
+    }).join("");
+  ;
+
+export default function (d: Document): void {
   const
     linkSectInner: HTMLElement = d.querySelector("#links>.section-inner") as HTMLElement
   ;
 
-  linkSectInner.innerHTML =
-  linksData.map((data: LinkGroup): string => {
-    return `
-      <div class="links-group">
-        <div class="links-title anim once fade float-up">${data.group}</div>
-        <ul class="nostyle">
-          ${
-            data.links.map(
-              (link: LinkGroupItem) => {
-                return `
-                  <li class="
-                      anim once
-                      link-item
-                      ${link.iconSlug || "default"}
-                    "
-                  >
-                    <a href="${link.url}"
-                      target="_blank"
-                      rel="me nofollow noopener noreferrer"
-                      referrerpolicy="no-referrer"
-                      aria-label="${link.name}"
-                    >
-                      <div class="social-icon">
-                        ${link.icon || `<i class="links" data-i="link"></i>`}
-                      </div>
+  linkSectInner.classList.remove("on");
 
-                      <span class="social-name">${link.name}</span>
+  setTimeout((): void => {
+    requestAnimationFrame((): void => {
+      linkSectInner.innerHTML = linksStrEl;
+      linkSectInner.classList.remove("has-loader");
 
-                      <small class="username">
-                        ${link.username || link.urlS}
-                      </small>
-                    </a>
-                  </li>
-                `;
-              }
-            ).join("")
-          }
-        </ul>
-      </div>
-      `;
-  }).join("");
+      initIcon();
 
-  initIcon();
+      linkSectInner.classList.add("on");
 
-  window.initAnim(linkSectInner);
-});}
+      window.initAnim(linkSectInner);
+    });
+  }, 500);
+}
