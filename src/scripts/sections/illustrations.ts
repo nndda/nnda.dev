9e5353569a7d1a80129b756462e71dcbdc895eb5
nnda <96333146+nndda@users.noreply.github.com>
2025-06-08 13:42:04 +0700

    Disable illustration effect functions entirely

--- src/scripts/sections/illustrations.ts
+++ src/scripts/sections/illustrations.ts
@@ -1,113 +1,113 @@
-interface illustElementData {
-  [elementIndex: number]: {
-    topRel: number,
-    height: number,
-    intersecting: boolean,
-  },
-}
+// interface illustElementData {
+//   [elementIndex: number]: {
+//     topRel: number,
+//     height: number,
+//     intersecting: boolean,
+//   },
+// }
 
-const
-  d: Document = document
-, documentWindow: Window = d.defaultView as Window
+// const
+//   d: Document = document
+// , documentWindow: Window = d.defaultView as Window
 
-, elements: NodeListOf<HTMLElement> = d.querySelectorAll("#illustrations .img > *")
+// , elements: NodeListOf<HTMLElement> = d.querySelectorAll("#illustrations .img > *")
 
-, illustData: illustElementData = { }
+// , illustData: illustElementData = { }
 
-, scrollOffset: number = .75
-, scrollOffsetV: number = (1 - scrollOffset) * .5
-;
+// , scrollOffset: number = .75
+// , scrollOffsetV: number = (1 - scrollOffset) * .5
+// ;
 
-function clamp(num: number, min: number, max: number): number {
-  return num <= min 
-    ? min 
-    : num >= max 
-      ? max 
-      : num
-}
+// function clamp(num: number, min: number, max: number): number {
+//   return num <= min 
+//     ? min 
+//     : num >= max 
+//       ? max 
+//       : num
+// }
 
-requestAnimationFrame(() => {
-  const
-    intersectionObserverOptions: Readonly<IntersectionObserverInit> = Object.freeze({
-      root: null,
-      threshold: 0,
-  });
+// requestAnimationFrame(() => {
+//   const
+//     intersectionObserverOptions: Readonly<IntersectionObserverInit> = Object.freeze({
+//       root: null,
+//       threshold: 0,
+//   });
 
-  function initializeIllustElement(i: number): void {
-    const rect: DOMRect = elements[i].getBoundingClientRect();
+//   function initializeIllustElement(i: number): void {
+//     const rect: DOMRect = elements[i].getBoundingClientRect();
 
-    illustData[i].height = rect.height;
+//     illustData[i].height = rect.height;
 
-    (elements[i].parentElement as HTMLElement).style.height = `${illustData[i].height * scrollOffset}px`;
-    elements[i].style.top = `-${illustData[i].height * scrollOffsetV}px`;
+//     (elements[i].parentElement as HTMLElement).style.height = `${illustData[i].height * scrollOffset}px`;
+//     elements[i].style.top = `-${illustData[i].height * scrollOffsetV}px`;
 
-    illustData[i].topRel = (rect.top + (illustData[i].height * .5)) + documentWindow.scrollY;
-  }
+//     illustData[i].topRel = (rect.top + (illustData[i].height * .5)) + documentWindow.scrollY;
+//   }
 
-  for (let i: number = elements.length; i-- > 0;) {
-    illustData[i] = {
-      topRel: 0,
-      height: 0,
-      intersecting: false,
-    };
+//   for (let i: number = elements.length; i-- > 0;) {
+//     illustData[i] = {
+//       topRel: 0,
+//       height: 0,
+//       intersecting: false,
+//     };
 
-    initializeIllustElement(i);
+//     initializeIllustElement(i);
 
-    // TODO
-    (elements[i] as HTMLImageElement).addEventListener("load", () => {
+//     // TODO
+//     (elements[i] as HTMLImageElement).addEventListener("load", () => {
 
-      initializeIllustElement(i);
+//       initializeIllustElement(i);
 
-    }, {
+//     }, {
 
-      once: true,
-      passive: true,
+//       once: true,
+//       passive: true,
 
-    });
+//     });
 
-    new IntersectionObserver((entries: IntersectionObserverEntry[]) => {
-        for (let k: number = entries.length; k-- > 0;) {
-          illustData[i].intersecting = entries[k].isIntersecting;
-        }
-      },
-      intersectionObserverOptions,
-    ).observe(elements[i]);
-  }
+//     new IntersectionObserver((entries: IntersectionObserverEntry[]) => {
+//         for (let k: number = entries.length; k-- > 0;) {
+//           illustData[i].intersecting = entries[k].isIntersecting;
+//         }
+//       },
+//       intersectionObserverOptions,
+//     ).observe(elements[i]);
+//   }
 
-  documentWindow.addEventListener("resize", () => {
-    for (let i: number = elements.length; i-- > 0;) {
-      initializeIllustElement(i);
-      scrollEv();
-    }
-  }, { passive: true });
-});
+//   documentWindow.addEventListener("resize", () => {
+//     for (let i: number = elements.length; i-- > 0;) {
+//       initializeIllustElement(i);
+//       scrollEv();
+//     }
+//   }, { passive: true });
+// });
 
-function updateIllustScroll(): void {
-  const
-    docScrollY: number = documentWindow.scrollY
-  , docInnerHeight: number = documentWindow.innerHeight
-  ;
+// function updateIllustScroll(): void {
+//   const
+//     docScrollY: number = documentWindow.scrollY
+//   , docInnerHeight: number = documentWindow.innerHeight
+//   ;
 
-  for (let i: number = elements.length; i-- > 0;) {
-    if (illustData[i].intersecting) {
-      elements[i].style.transform = `translateY(${
-        clamp(
-          1 - ( 2 * (illustData[i].topRel - docScrollY) + illustData[i].height ) / docInnerHeight,
-          -1, 1,
-        )
-          * 
-        (
-          illustData[i].height * scrollOffsetV
-        )
-      }px)`;
-    }
-  }
-}
+//   for (let i: number = elements.length; i-- > 0;) {
+//     if (illustData[i].intersecting) {
+//       elements[i].style.transform = `translateY(${
+//         clamp(
+//           1 - ( 2 * (illustData[i].topRel - docScrollY) + illustData[i].height ) / docInnerHeight,
+//           -1, 1,
+//         )
+//           * 
+//         (
+//           illustData[i].height * scrollOffsetV
+//         )
+//       }px)`;
+//     }
+//   }
+// }
 
-function scrollEv(): void {
-  requestAnimationFrame(updateIllustScroll);
-}
+// function scrollEv(): void {
+//   requestAnimationFrame(updateIllustScroll);
+// }
 
-scrollEv();
+// scrollEv();
 
-documentWindow.addEventListener("scroll", scrollEv, { passive: true });
+// documentWindow.addEventListener("scroll", scrollEv, { passive: true });
