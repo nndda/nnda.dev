ac5a329a3c86ebd7462b1dffa9c7983921ad4900
nnda <96333146+nndda@users.noreply.github.com>
2025-09-29 18:22:25 +0700

    Refactor profile section

--- src/scripts/sections/profile.ts
+++ src/scripts/sections/profile.ts
@@ -1,24 +1,23 @@
 import initIcon from "../build/icons/profile";
 
+initIcon();
+
+const
+  timezoneClock: HTMLElement = document.getElementById("clock-timezone") as HTMLElement
+, timezoneOpt: Intl.DateTimeFormatOptions = {
+    timeZone: timezoneClock.getAttribute("data-timezone") as string,
+    hour12: false,
+  }
+;
+
+function updateClock(): void {
+  timezoneClock.textContent = new Date().toLocaleTimeString("en-US", timezoneOpt);
+}
+
+updateClock();
+setInterval(updateClock, 1e3);
+
 window.initAnim(
   document.getElementById("profile") as HTMLElement,
   window.matchMedia("media (width >= 700px) and (height >= 920px)").matches ? "-210px 0px" : "0px 0px",
-  (): void => {
-    initIcon();
-
-    const
-      timezoneClock: HTMLElement = document.getElementById("clock-timezone") as HTMLElement
-    , timezoneOpt: Intl.DateTimeFormatOptions = {
-        timeZone: timezoneClock.getAttribute("data-timezone") as string,
-        hour12: false,
-      }
-    ;
-
-    function updateClock(): void {
-      timezoneClock.textContent = new Date().toLocaleTimeString("en-US", timezoneOpt);
-    }
-
-    updateClock();
-    setInterval(updateClock, 1e3);
-  },
 );
