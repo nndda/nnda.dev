3d7b6ff4f5050cd4ed6c4dd3c4d7a1360c432888
nnda <git@nnda.dev>
2026-09-21 05:38:43 +0700

    feat: create dedicated link & profile page

---
+++ src/scripts/_pages/links.ts
@@ -0,0 +1,8 @@
+// import { loadCSS } from "../framework";
+import "../sections/links";
+
+// import styleLinks from "../../styles/sections/_links.lazy.scss?url";
+
+// loadCSS(styleLinks).then(() => {
+//   import("../sections/links");
+// });
